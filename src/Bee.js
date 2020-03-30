424959f3038c82810e0392506c436883ec4ded08
clutejoshua <57841091+clutejoshua@users.noreply.github.com>
2020-03-30 10:22:00 -0700

    Finished Grub working on Bee.js

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,15 @@
 var Bee = function() {
+  this.age = 5;
+  this.color = 'yellow';
+  this.food = Grub.call(this, food);
+
+
+
+
 };
 
+
+
+
+Bee.prototype = new Grub();
+Bee.prototype.constructor = Bee;
