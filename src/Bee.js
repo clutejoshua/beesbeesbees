1296df83aae9467d5c21839cdd326d72b57c2908
clutejoshua <57841091+clutejoshua@users.noreply.github.com>
2020-03-30 10:27:29 -0700

    Used the Grub.call and prototype new and concstructor to create Bee inheritance

--- src/Bee.js
+++ src/Bee.js
@@ -1,15 +1,9 @@
 var Bee = function() {
+  Grub.call(this);
   this.age = 5;
   this.color = 'yellow';
-  this.food = Grub.call(this, food);
-
-
-
-
+  this.job = 'keep on growing';
 };
-
-
-
 
 Bee.prototype = new Grub();
 Bee.prototype.constructor = Bee;
